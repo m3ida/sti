4978bc4c8b4b73fde11460e7a435933d05d019e9
André Almeida <andrealmeida@vipaconnect.com>
2023-05-24 18:00:45 +0100

    changes

--- lib/player.dart
+++ lib/player.dart
@@ -54,110 +54,190 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen>
           : AppBar(
               title: Text(widget.video.title),
             ),
-      body: Builder(builder: (context) {
-        if (!_controller.value.isInitialized) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        return FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Center(
-                child: Column(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: RotationTransition(
-                        turns: Tween(begin: 0.0, end: 1.0).animate(
+      body: SingleChildScrollView(
+        child: Builder(builder: (context) {
+          if (!_controller.value.isInitialized) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Center(
+                  child: Column(
+                    children: [
+                      LayoutBuilder(builder: (context, constraints) {
+                        return RotationTransition(
+                          turns: Tween(begin: 0.0, end: 1.0).animate(
                             CurvedAnimation(
                                 parent: _animationController,
-                                curve: Curves.linear)),
-                        child: AspectRatio(
-                          aspectRatio: _aspectRatio,
-                          child: VideoPlayer(_controller),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              if (_controller.value.isPlaying) {
-                                _controller.pause();
-                              } else {
-                                _controller.play();
-                              }
-                            });
-                          },
-                          icon: Icon(
-                            _controller.value.isPlaying
-                                ? Icons.pause
-                                : Icons.play_arrow,
+                                curve: Curves.linear),
                           ),
-                        ),
-                        Text(_controller.value.position
-                            .toString()
-                            .substring(0, 7)),
-                        Expanded(
-                          child: Slider(
-                            value:
-                                _controller.value.position.inSeconds.toDouble(),
-                            min: 0.0,
-                            max:
-                                _controller.value.duration.inSeconds.toDouble(),
-                            onChanged: (value) {
+                          child: Container(
+                            height: _isFullScreen
+                                ? MediaQuery.of(context).size.height
+                                : null,
+                            width: MediaQuery.of(context).size.width,
+                            alignment: Alignment.center,
+                            color: Colors.black,
+                            child: AspectRatio(
+                              aspectRatio: 16 / 9,
+                              child: Stack(
+                                children: [
+                                  VideoPlayer(_controller),
+                                  Positioned(
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: GestureDetector(
+                                            behavior:
+                                                HitTestBehavior.translucent,
+                                            onTap: () {
+                                              setState(() {
+                                                if (_controller
+                                                    .value.isPlaying) {
+                                                  _controller.pause();
+                                                } else {
+                                                  _controller.play();
+                                                }
+                                              });
+                                            },
+                                            onDoubleTap: () {
+                                              final newPos = _controller
+                                                      .value.position -
+                                                  const Duration(seconds: 10);
+                                              _controller.seekTo(newPos);
+                                            },
+                                            child: Container(
+                                                color: Colors.transparent),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: GestureDetector(
+                                            behavior:
+                                                HitTestBehavior.translucent,
+                                            onTap: () {
+                                              setState(() {
+                                                if (_controller
+                                                    .value.isPlaying) {
+                                                  _controller.pause();
+                                                } else {
+                                                  _controller.play();
+                                                }
+                                              });
+                                            },
+                                            onDoubleTap: () {
+                                              final newPos = _controller
+                                                      .value.position +
+                                                  const Duration(seconds: 10);
+                                              _controller.seekTo(newPos);
+                                            },
+                                            child: Container(
+                                                color: Colors.transparent),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
                               setState(() {
-                                _controller
-                                    .seekTo(Duration(seconds: value.toInt()));
+                                if (_controller.value.isPlaying) {
+                                  _controller.pause();
+                                } else {
+                                  _controller.play();
+                                }
                               });
                             },
+                            icon: Icon(
+                              _controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                            ),
                           ),
-                        ),
-                        // IconButton(
-                        //   onPressed: () {
-                        //     setState(() {
-                        //       _isFullScreen = !_isFullScreen;
-                        //       if (_isFullScreen) {
-                        //         SystemChrome.setPreferredOrientations([
-                        //           DeviceOrientation.landscapeLeft,
-                        //           DeviceOrientation.landscapeRight,
-                        //         ]);
-                        //         SystemChrome.setEnabledSystemUIOverlays([]);
-                        //         _aspectRatio = MediaQuery.of(context).size.width /
-                        //             MediaQuery.of(context).size.height;
-                        //         _animationController.forward();
-                        //       } else {
-                        //         SystemChrome.setPreferredOrientations([
-                        //           DeviceOrientation.portraitUp,
-                        //           DeviceOrientation.portraitDown,
-                        //         ]);
-                        //         SystemChrome.setEnabledSystemUIOverlays(
-                        //             [SystemUiOverlay.bottom, SystemUiOverlay.top]);
-                        //         _aspectRatio = 16 / 9;
-                        //         _animationController.reverse();
-                        //       }
-                        //     });
-                        //   },
-                        //   icon: const Icon(Icons.fullscreen),
-                        // )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Text(widget.video.description),
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        );
-      }),
+                          Text(_controller.value.position
+                              .toString()
+                              .substring(0, 7)),
+                          const Text('/'),
+                          Text(_controller.value.duration
+                              .toString()
+                              .substring(0, 7)),
+                          Expanded(
+                            child: Slider(
+                              value: _controller.value.position.inSeconds
+                                  .toDouble(),
+                              min: 0.0,
+                              max: _controller.value.duration.inSeconds
+                                  .toDouble(),
+                              onChanged: (value) {
+                                setState(() {
+                                  _controller
+                                      .seekTo(Duration(seconds: value.toInt()));
+                                });
+                              },
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _isFullScreen = !_isFullScreen;
+                                if (_isFullScreen) {
+                                  SystemChrome.setPreferredOrientations([
+                                    DeviceOrientation.landscapeLeft,
+                                    DeviceOrientation.landscapeRight,
+                                  ]);
+                                  // SystemChrome.setEnabledSystemUIOverlays([]);
+                                  SystemChrome.setEnabledSystemUIMode(
+                                      SystemUiMode.immersive);
+                                  _aspectRatio =
+                                      MediaQuery.of(context).size.width /
+                                          MediaQuery.of(context).size.height;
+                                  _animationController.forward();
+                                } else {
+                                  SystemChrome.setPreferredOrientations([
+                                    DeviceOrientation.portraitUp,
+                                    DeviceOrientation.portraitDown,
+                                  ]);
+                                  SystemChrome.setEnabledSystemUIMode(
+                                      SystemUiMode.manual,
+                                      overlays: [
+                                        SystemUiOverlay.top,
+                                        SystemUiOverlay.bottom
+                                      ]);
+
+                                  _aspectRatio = 16 / 9;
+                                  _animationController.reverse();
+                                }
+                              });
+                            },
+                            icon: const Icon(Icons.fullscreen),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(widget.video.description),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          );
+        }),
+      ),
     );
   }
 }
--- lib/videocard.dart
+++ lib/videocard.dart
@@ -1,11 +1,39 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sti_project/player.dart';
 import 'package:sti_project/video.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoCard extends StatelessWidget {
+class VideoCard extends StatefulWidget {
   final Video video;
 
   const VideoCard({super.key, required this.video});
+
+  @override
+  State<VideoCard> createState() => _VideoCardState();
+}
+
+class _VideoCardState extends State<VideoCard>
+    with SingleTickerProviderStateMixin {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.video.sources);
+    //  _initializeVideoPlayerFuture = _controller.initialize();
+    _initializeVideoPlayerFuture = _controller.initialize().then((_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +43,7 @@ class VideoCard extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => VideoPlayerScreen(video: video),
+            builder: (context) => VideoPlayerScreen(video: widget.video),
           ),
         );
       },
@@ -27,7 +55,7 @@ class VideoCard extends StatelessWidget {
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.4,
               child: Image(
-                image: NetworkImage(video.thumb),
+                image: NetworkImage(widget.video.thumb),
                 fit: BoxFit.cover,
                 height: 80,
               ),
@@ -36,17 +64,28 @@ class VideoCard extends StatelessWidget {
               width: MediaQuery.of(context).size.width * 0.5,
               padding: const EdgeInsets.only(left: 10),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(video.title.length > 30
-                      ? '${video.title.substring(0, 30)}...'
-                      : video.title),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(widget.video.title.length > 30
+                          ? '${widget.video.title.substring(0, 30)}...'
+                          : widget.video.title),
+                      Text(
+                        widget.video.description.length > 100
+                            ? '${widget.video.description.substring(0, 100)}...'
+                            : widget.video.description,
+                        overflow: TextOverflow.clip,
+                      ),
+                    ],
+                  ),
                   Text(
-                    video.description.length > 100
-                        ? '${video.description.substring(0, 100)}...'
-                        : video.description,
-                    overflow: TextOverflow.clip,
+                    _controller.value.duration.toString().substring(0, 7),
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(color: Colors.black),
                   ),
                 ],
               ),
